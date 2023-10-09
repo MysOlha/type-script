dcb11508b5ac9a992635155374da56549d4fc79e
NPC-Roman <misarosh70@gmail.com>
2023-10-09 10:21:56 +0300

    practice

---
+++ types_practice.ts
@@ -0,0 +1,17 @@
+type User = {
+    name: string;
+    age: number;
+  };
+  
+  let user: User = {
+    name: 'Tom',
+    age: 30,
+  };
+  
+  let userJack: User = {
+    name: 'Jack',
+    age: 25,
+  };
+  
+  export {};
+  
